3a2834a4b5fdd43811b51d3d82a0e9596fbf2f2e
Aigbadonbrianehizele <ttops54321@gmail.com>
2026-04-02 16:50:36 +0100

    list manipulation basics, broke IndexError, fixed

---
+++ lists.py
@@ -0,0 +1,7 @@
+sentences = ["I love this","This is bad","Great job","So Bad","Okay"]
+labels = [1,0,1,0,1]
+print(sentences[0])
+print(sentences[1])
+labels[1] = 1
+print(labels[1])
+print(sentences[10])
