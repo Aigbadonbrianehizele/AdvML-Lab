2936027101da0ebaf0ce747b34c42c535d764680
Aigbadonbrianehizele <ttops54321@gmail.com>
2026-04-03 08:32:44 +0100

    for loop + enumerate, broke IndentationError, fixed

--- loops.py
+++ loops.py
@@ -1,4 +1,4 @@
 sentences = ["I love this","This is bad","Great job","Okay"]
 labels = [1,0,1,0,1]
-for i,sentence in  enumerate(labels):
+for i,sentence in enumerate(labels):
     print(i,sentence,labels[i])
