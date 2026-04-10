f39ce46267576c1f884e53bb39f0312d92e29d31
Aigbadonbrianehizele <ttops54321@gmail.com>
2026-04-10 11:24:59 +0100

    Add files via upload
    Day3

---
+++ ROT47_O1/SYS_ARGV.py
@@ -0,0 +1,12 @@
+import sys
+
+if len(sys.argv) == 4:
+    print(f"WE ARE PRINTING GUYS MISSIO SUCCESSFUL <greeting> <greeting_2> <cluster>!!!")
+else:
+    print("error")
+greeting = sys.argv[1]
+greeting_2 = sys.argv[2]
+cluster = sys.argv[3]
+
+print("HAAAAAAAA!!!!!!!!!!!!!!!!")
+print(f"HAHA {greeting} {greeting_2} {cluster}")
